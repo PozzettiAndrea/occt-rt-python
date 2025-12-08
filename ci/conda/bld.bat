45f14efb31160d5983c504e29ac8a1949f02a913
Andrea Pozzetti <andrea@pozzetti.it>
2025-12-08 20:48:31 +0000

    bundling

--- ci/conda/bld.bat
+++ ci/conda/bld.bat
@@ -17,3 +17,21 @@ if errorlevel 1 exit 1
 
 xcopy /E /I occt_rt %SP_DIR%\occt_rt
 if errorlevel 1 exit 1
+
+REM Bundle required OCCT DLLs into the package (NVIDIA-style)
+mkdir %SP_DIR%\occt_rt\libs
+copy %LIBRARY_BIN%\TKernel.dll %SP_DIR%\occt_rt\libs\
+copy %LIBRARY_BIN%\TKMath.dll %SP_DIR%\occt_rt\libs\
+copy %LIBRARY_BIN%\TKG2d.dll %SP_DIR%\occt_rt\libs\
+copy %LIBRARY_BIN%\TKG3d.dll %SP_DIR%\occt_rt\libs\
+copy %LIBRARY_BIN%\TKGeomBase.dll %SP_DIR%\occt_rt\libs\
+copy %LIBRARY_BIN%\TKBRep.dll %SP_DIR%\occt_rt\libs\
+copy %LIBRARY_BIN%\TKGeomAlgo.dll %SP_DIR%\occt_rt\libs\
+copy %LIBRARY_BIN%\TKTopAlgo.dll %SP_DIR%\occt_rt\libs\
+copy %LIBRARY_BIN%\TKMesh.dll %SP_DIR%\occt_rt\libs\
+copy %LIBRARY_BIN%\TKShHealing.dll %SP_DIR%\occt_rt\libs\
+copy %LIBRARY_BIN%\TKPrim.dll %SP_DIR%\occt_rt\libs\
+REM Copy TBB runtime
+copy %LIBRARY_BIN%\tbb12.dll %SP_DIR%\occt_rt\libs\ 2>nul
+REM Copy Embree if present
+copy %LIBRARY_BIN%\embree4.dll %SP_DIR%\occt_rt\libs\ 2>nul
