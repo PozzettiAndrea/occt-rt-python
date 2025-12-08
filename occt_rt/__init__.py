af57e02504e2c6d9c23c83dbee3edd8bce25717b
Andrea Pozzetti <andrea@pozzetti.it>
2025-12-08 20:47:03 +0000

    bundling dlls

--- occt_rt/__init__.py
+++ occt_rt/__init__.py
@@ -12,12 +12,35 @@ Example:
     >>> result = rt.cast_ray(origin=(5, 5, 100), direction=(0, 0, -1))
     >>> print(result)
     {'hit': True, 'point': array([5., 5., 10.]), 'normal': array([0., 0., 1.]), ...}
-
 """
+import os
+import sys
+import ctypes
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __author__ = "Andrea Pozzetti"
 __license__ = "LGPL-2.1"
+
+# Add bundled libs to search path BEFORE importing _OCCTRT
+_libs_dir = os.path.join(os.path.dirname(__file__), 'libs')
+
+if sys.platform == 'win32' and os.path.isdir(_libs_dir):
+    # Windows: Use add_dll_directory (Python 3.8+) and PATH
+    if hasattr(os, 'add_dll_directory'):
+        os.add_dll_directory(_libs_dir)
+    os.environ['PATH'] = _libs_dir + os.pathsep + os.environ.get('PATH', '')
+
+elif sys.platform == 'linux' and os.path.isdir(_libs_dir):
+    # Linux: Preload bundled shared libraries
+    for lib_name in ['libTKernel.so', 'libTKMath.so', 'libTKG2d.so', 'libTKG3d.so',
+                     'libTKGeomBase.so', 'libTKBRep.so', 'libTKGeomAlgo.so',
+                     'libTKTopAlgo.so', 'libTKMesh.so']:
+        lib_path = os.path.join(_libs_dir, lib_name)
+        if os.path.exists(lib_path):
+            try:
+                ctypes.CDLL(lib_path, mode=ctypes.RTLD_GLOBAL)
+            except OSError:
+                pass
 
 from .raytracer import Raytracer, Backend
 
